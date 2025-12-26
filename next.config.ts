ba64f352ea9fcbbe28a8ec1fcf83c6abb081d1a2
warlei07 <warlei.rocha@digitalrepublic.com.br>
2025-12-26 19:26:40 -0300

    gitHub page up

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Habilita exportação estática
+  images: {
+    unoptimized: true, // Necessário para GitHub Pages
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
